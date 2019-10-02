3ffdd20b99e90d9e7b2babeb4dff841f5640a828
sahand504 <asri.sahand@gmail.com>
2019-10-02 15:18:34 -0400

    complete the naive bayes classifier for newsgroup messages

--- mytokenizer.py
+++ mytokenizer.py
@@ -15,14 +15,16 @@ def remove_digits(str):
 
 
 def get_files_lines(dir_name):
+    file_names = []
     files_lines = []
     for root, dirs, files in os.walk(dir_name):
         for filename in files:
             path = os.path.join(root, filename)
             with open(path) as file:
                 file_str = file.read().splitlines()
+                file_names.append(filename)
                 files_lines.append(remove_headers(file_str))
-    return files_lines
+    return file_names, files_lines
 
 
 stop_words = set(nltk.corpus.stopwords.words('english'))
@@ -46,3 +48,25 @@ def tokenize(files_lines):
                     filtered_tokens.append(w)
     return filtered_tokens
 
+
+def test_tokenize(files_lines):
+    print(files_lines[0])
+    filtered_files_tokens = []
+    for file_lines in files_lines:
+        filtered_file_tokens = []
+        for line in file_lines:
+            # Convert every string to lowercase
+            line = line.lower()
+            # Remove digits
+            line = remove_digits(line)
+
+            # Filter Punctuation
+            tokenizer = nltk.RegexpTokenizer(r'\w+')
+            word_tokens = tokenizer.tokenize(line)
+
+            # Filter Stopwords and digits
+            for w in word_tokens:
+                if w not in stop_words:
+                    filtered_file_tokens.append(w)
+        filtered_files_tokens.append(filtered_file_tokens)
+    return filtered_files_tokens
--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -2,6 +2,7 @@ import os
 import operator
 import nltk
 import mytokenizer
+import math
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -15,16 +16,15 @@ COMP_TEST_DIR = os.path.join(TEST_DIR, "comp.sys.ibm.pc.hardware")
 ELEC_TRAIN_DIR = os.path.join(TRAIN_DIR, "sci.electronics")
 ELEC_TEST_DIR = os.path.join(TEST_DIR, "sci.electronics")
 
-
-comp_train_flines = mytokenizer.get_files_lines(COMP_TRAIN_DIR)
-elec_train_flines = mytokenizer.get_files_lines(ELEC_TRAIN_DIR)
-comp_test_flines = mytokenizer.get_files_lines(COMP_TEST_DIR)
-elec_test_flines = mytokenizer.get_files_lines(ELEC_TEST_DIR)
+comp_train_fnames, comp_train_flines = mytokenizer.get_files_lines(COMP_TRAIN_DIR)
+elec_train_fnames, elec_train_flines = mytokenizer.get_files_lines(ELEC_TRAIN_DIR)
+comp_test_fnames, comp_test_flines = mytokenizer.get_files_lines(COMP_TEST_DIR)
+elec_test_fnames, elec_test_flines = mytokenizer.get_files_lines(ELEC_TEST_DIR)
 
 comp_train_tokens = mytokenizer.tokenize(comp_train_flines)
 elec_train_tokens = mytokenizer.tokenize(elec_train_flines)
-comp_test_tokens = mytokenizer.tokenize(comp_test_flines)
-elec_test_tokens = mytokenizer.tokenize(elec_test_flines)
+comp_test_files_tokens = mytokenizer.test_tokenize(comp_test_flines)
+elec_test_files_tokens = mytokenizer.test_tokenize(elec_test_flines)
 
 comp_train_token_dic = {}
 elec_train_token_dic = {}
@@ -33,35 +33,67 @@ elec_test_token_dic = {}
 
 for token in comp_train_tokens:
     if token not in comp_train_token_dic:
-        comp_train_token_dic[token] = 0
+        comp_train_token_dic[token] = 1
     else:
         comp_train_token_dic[token] += 1
 
 for token in elec_train_tokens:
     if token not in elec_train_token_dic:
-        elec_train_token_dic[token] = 0
+        elec_train_token_dic[token] = 1
     else:
         elec_train_token_dic[token] += 1
 
-for token in comp_test_tokens:
-    if token not in comp_test_token_dic:
-        comp_test_token_dic[token] = 0
-    else:
-        comp_test_token_dic[token] += 1
-
-for token in elec_test_tokens:
-    if token not in elec_test_token_dic:
-        elec_test_token_dic[token] = 0
-    else:
-        elec_test_token_dic[token] += 1
-
-
-comp_train_token_dic = sorted(comp_train_token_dic.items(), key=operator.itemgetter(1), reverse=True)
-elec_train_token_dic = sorted(elec_train_token_dic.items(), key=operator.itemgetter(1), reverse=True)
-comp_test_token_dic = sorted(comp_test_token_dic.items(), key=operator.itemgetter(1), reverse=True)
-elec_test_token_dic = sorted(elec_test_token_dic.items(), key=operator.itemgetter(1), reverse=True)
+# print(type(comp_train_token_dic))
+# comp_train_token_dic = sorted(comp_train_token_dic.items(), key=operator.itemgetter(1), reverse=True)
+# elec_train_token_dic = sorted(elec_train_token_dic.items(), key=operator.itemgetter(1), reverse=True)
+# print(type(comp_train_token_dic))
 
 print(comp_train_token_dic)
 print(elec_train_token_dic)
-print(comp_test_token_dic)
-print(elec_test_token_dic)
+print(comp_test_files_tokens)
+print(elec_test_files_tokens)
+
+p_comp = len(comp_train_token_dic) / (len(comp_train_token_dic) + len(elec_train_token_dic))
+p_elec = 1 - p_comp
+
+c_comp = math.log2(p_comp)
+c_elec = math.log2(p_elec)
+
+print(math.log2(p_comp))
+print(math.log2(p_elec))
+
+missclass_counter = 0
+for file_idx, comp_test_file_tokens in enumerate(comp_test_files_tokens):
+    c_comp = math.log2(p_comp)
+    c_elec = math.log2(p_elec)
+    for comp_test_file_token in comp_test_file_tokens:
+        if comp_test_file_token in comp_train_token_dic:
+            c_comp += math.log2(comp_train_token_dic[comp_test_file_token])
+        if comp_test_file_token in elec_train_token_dic:
+            c_elec += math.log2(elec_train_token_dic[comp_test_file_token])
+    if c_elec > c_comp:
+        print("Miss Classified! Test Filename --> " + comp_test_fnames[file_idx])
+        print("c_comp: " + str(c_comp))
+        print("c_elec: " + str(c_elec))
+        missclass_counter += 1
+
+for file_idx, elec_test_file_tokens in enumerate(elec_test_files_tokens):
+    c_comp = math.log2(p_comp)
+    c_elec = math.log2(p_elec)
+    for elec_test_file_token in elec_test_file_tokens:
+        if elec_test_file_token in comp_train_token_dic:
+            c_comp += math.log2(comp_train_token_dic[elec_test_file_token])
+        if elec_test_file_token in elec_train_token_dic:
+            c_elec += math.log2(elec_train_token_dic[elec_test_file_token])
+    if c_elec < c_comp:
+        print("Miss Classified! Test Filename --> " + elec_test_fnames[file_idx])
+        print("c_comp: " + str(c_comp))
+        print("c_elec: " + str(c_elec))
+        print("-------------------------------------------------------------------")
+        missclass_counter += 1
+
+test_num = (len(comp_test_fnames) + len(elec_test_fnames))
+
+correctclass_counter = test_num - missclass_counter
+print("Missclassified Counter = " + str(missclass_counter))
+print("Precision = " + str((correctclass_counter / test_num) * 100))
